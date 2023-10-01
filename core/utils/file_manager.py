ebfcb43f0db45fe5f7cc3ed0bafa2eb929f17ae6
MsLolita <oblikovuizapuc@gmail.com>
2023-10-01 15:09:34 +0300

    upd file_manager.py

--- core/utils/file_manager.py
+++ core/utils/file_manager.py
@@ -1,3 +1,21 @@
+from typing import Optional
+
+
+def file_to_list(
+        filename: str
+):
+    with open(filename, 'r+') as f:
+        return list(filter(bool, f.read().splitlines()))
+
+
+def str_to_file(file_name: str, msg: str, mode: Optional[str] = "a"):
+    with open(
+            file_name,
+            mode
+    ) as text_file:
+        text_file.write(f"{msg}\n")
+
+
 def shift_file(file):
     with open(file, 'r+') as f:  # open file in read / write mode
         first_line = f.readline()  # read the first line and throw it out
